393e41f38bbf236f79443209d922dd086fd847db
fibin4056x <fibinkunnath@gmail.com>
2025-08-01 15:31:36 +0530

    day1

---
+++ y/src/blog/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {NavLink } from 'react-router-dom'
+import Navbar from './navbar'
+
+function Homepage() {
+  return (
+    <div>
+       hello welcme
+    </div>
+  )
+}
+
+export default Homepage
